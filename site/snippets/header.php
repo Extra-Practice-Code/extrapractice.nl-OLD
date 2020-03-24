e969d77b360754a0479a04eb4e9d1defd142ac5d
benjaminearl <bnjmnearl@gmail.com>
2020-03-24 10:57:55 +0100

    change title of site

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,9 +3,7 @@
   <meta charset="UTF-8">
   <meta name="description" content="<?= $site->description() ?>">
   <meta name="keywords" content="<?= $site->keywords() ?>">
-  <title>
-    <?= $page->title() ?> | <?= $site->title() ?>
-  </title>
+  <title><?= $site->title() ?></title>
   <link rel="shortcut icon" href="assets/images/favicon/favicon.ico" type="image/x-icon">
   <?= css('assets/css/index.css') ?>
   <?= js('assets/js/script.js') ?>
