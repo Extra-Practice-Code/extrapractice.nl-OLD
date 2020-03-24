21d74cc67aa2d6e93818fb659709c829b0f95520
benjaminearl <bnjmnearl@gmail.com>
2020-03-24 11:18:11 +0100

    add favicon

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,7 +4,13 @@
   <meta name="description" content="<?= $site->description() ?>">
   <meta name="keywords" content="<?= $site->keywords() ?>">
   <title><?= $site->title() ?></title>
-  <link rel="shortcut icon" href="assets/images/favicon/favicon.ico" type="image/x-icon">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+  <link rel="manifest" href="assets/images/favicon/site.webmanifest">
+  <link rel="mask-icon" href="assets/images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#ffc40d">
+  <meta name="theme-color" content="#ffffff">
   <?= css('assets/css/index.css') ?>
   <?= js('assets/js/script.js') ?>
   <?= js('assets/js/jquery.min.js') ?>
